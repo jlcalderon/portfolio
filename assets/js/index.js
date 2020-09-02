e0ec706dd058641231e33d9175b22233f3b4fa60
JLC <jlc@annonymous.com>
2020-09-02 01:06:53 -0500

    Adding responsive behaviours

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,24 @@
-$('a').click(function() {
-    var top = $('body').find($(this).attr('href')).offset().top;
-    $('html, body').animate({
-        scrollTop: top
-    }, 400, 'easeOutExpo');
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $(".innernav").on('click', function(event) {
 
-    return false;
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
