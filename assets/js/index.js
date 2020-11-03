48a7bf8a48a8982ae88298b0f981883e6312ef1b
JLC <jlc@annonymous.com>
2020-11-03 10:55:31 -0600

    improving UX in portfolio site and updating information in about me section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,20 @@
 $(document).ready(function() {
-    // Add smooth scrolling to all links
-    $(".innernav").on('click', function(event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
+    // Displaying and hiding top button on user scroll windows
+    // When the user scrolls down 20px from the top of the document, show the button
+    $(window).on("scroll", document, function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $("#goTop").show();
+        } else {
+            $("#goTop").hide();
+        }
     });
+
+
+    $(document).on("click", "#goTop", function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
+
 });
